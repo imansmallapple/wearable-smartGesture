40a2f77ba78bd519652002a768240ebd6e83da61
Chen <chensong64@h-partners.com>
2025-10-20 16:32:51 +0200

    Create template
    Signed-off-by: Chen <chensong64@h-partners.com>

---
+++ entry/src/main/ets/common/constant.ts
@@ -0,0 +1,67 @@
+import Touch from "../models/model"
+
+export default class Constant {
+  static crownAbilityList: string[] = [
+    'Press',
+    'Press and hold',
+    'Press Launcher',
+    'Press and hold for 3s',
+    'Double press',
+    'Turn counterclockwise',
+    'Turn clockwise',
+  ]
+
+  static gestureList: Touch[] = [
+    {
+      title: "Touch",
+      description: "Touch an element to activate the component or function",
+      task: "Touch the toggle to control the bulb",
+      image: 'app.media.touch'
+    },
+    {
+      title: "Press and hold",
+      description: "Press and hold an element to display additional controls or functions",
+      task: "Task",
+      image: 'app.media.presshold'
+    },
+
+    {
+      title: "Drag and drop",
+      description: "Drag an element to move it",
+      task: "Task",
+      image: 'app.media.dragdrop'
+    },
+
+    {
+      title: "Swipe",
+      description: "Scroll the content vertically or swipe the screen horizontally",
+      task: "Task",
+      image: 'app.media.swipe'
+    },
+
+    {
+      title: "Double-tap",
+      description: "Double-tap to zoom in or zoom out on the content",
+      task: "Task",
+      image: 'app.media.double_tap'
+    },
+    {
+      title: "Pinch",
+      description: "Pinch to zoom in or zoom out on the content",
+      task: "Task",
+      image: 'app.media.pinch'
+    },
+    {
+      title: "Rotate",
+      description: "Rotate the selected content",
+      task: "Task",
+      image: 'app.media.rotate'
+    },
+    {
+      title: "Tap",
+      description: "Tap the screen with a single knuckle to take a screenshot",
+      task: "Task",
+      image: 'app.media.tap'
+    },
+  ]
+}
